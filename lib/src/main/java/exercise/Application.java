aebab4cbc25f504bc146b1db5f1caa00cf705fbf
ghutterer <gregor.hutterer@hotmail.com>
2021-09-30 13:21:56 +0200

    submission

--- lib/src/main/java/exercise/Application.java
+++ lib/src/main/java/exercise/Application.java
@@ -11,7 +11,7 @@ public class Application {
 	}
 
 	public static void printArray(int[] numbers) {
-		for (int i = 0; i < numbers.length + 1; i++) {
+		for (int i = 0; i < numbers.length; i++) {
 			System.out.println(numbers[i]);
 		}
 	}
